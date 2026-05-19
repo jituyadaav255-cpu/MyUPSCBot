d2acff3da8f6865b27e901a793a5986376430853
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 17:51:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from pyrogram import Client
 from vars import BOT_TOKEN
-app = Client("my_bot", bot_token=BOT_TOKEN)
-print("बोट लाइव है!")
+
+# API_ID और HASH को None पर सेट करना सबसे जरूरी है
+# ताकि बोट सिर्फ TOKEN का इस्तेमाल करे
+app = Client(
+    "my_bot",
+    api_id=None,
+    api_hash=None,
+    bot_token=BOT_TOKEN
+)
+
+print("बोट शुरू हो रहा है...")
 app.run()
