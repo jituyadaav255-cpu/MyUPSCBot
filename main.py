dab0dec5971957cdf31c284a85269ec7d14e888c
jituyadaav255-cpu <jituyadaav255@gmail.com>
2026-05-19 18:12:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from telethon import TelegramClient
 from vars import BOT_TOKEN
 
-# Telethon के लिए API_ID और HASH की जरूरत नहीं होती अगर आप सिर्फ बोट टोकन यूज़ कर रहे हैं
-# यहाँ API_ID में 12345 और HASH में कोई भी रैंडम स्ट्रिंग डाल दें, यह बस एक फॉर्मेलिटी है
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
+# अपनी असली डिटेल्स यहाँ डालें
+api_id = 31911187
+api_hash = '8291ae3d580f1fb5f8f84e0e3c6a3e6f'
 
+# बोट स्टार्ट करें
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=BOT_TOKEN)
 
 print("बोट सफलता के साथ लाइव हो गया है!")
